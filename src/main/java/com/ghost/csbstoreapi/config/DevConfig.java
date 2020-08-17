56d1f55cefbded5cc1dd3ff991d9e1cd53677754
Saulo Tavares <saulo.11@hotmail.com>
2020-08-17 01:19:15 -0300

    Added SmtpEmailService with Google Smtp Server

--- src/main/java/com/ghost/csbstoreapi/config/DevConfig.java
+++ src/main/java/com/ghost/csbstoreapi/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.ghost.csbstoreapi.config;
 
 import com.ghost.csbstoreapi.services.DBService;
+import com.ghost.csbstoreapi.services.Email.EmailService;
+import com.ghost.csbstoreapi.services.Email.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -27,5 +29,10 @@ public class DevConfig {
         }
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService(){
+        return new SmtpEmailService();
     }
 }
